ada4123492da780ab16432a2f553538d7835773d
KalsonSaint <saintkalson@gmail.com>
2021-11-15 03:26:24 +0100

    nav inProgress

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,8 +1,31 @@
-import React from "react";
-import { RIMenu3Line, RiCloseLine } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/images/logo.svg";
 
+const Menu = () => (
+  <>
+    {" "}
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt">What is GPT?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
+
 const Nav = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt__navbar">
       <div className="gpt__navbar-links">
@@ -10,24 +33,35 @@ const Nav = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#wgpt">What is GPT?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          <Menu />
         </div>
       </div>
-      <div className="gpt__navbar-sign"></div>
+      <div className="gpt__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="gpt__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu("false")}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu("true")}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt__navbar-menu_container">
+            <div className="gpt__navbar-menu_container-links">
+              <Menu />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
